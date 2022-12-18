2b638d0b8c79fc188185d46c84794390316cd57f
Joseph McPartland <joemcpartland@gmail.com>
2022-12-18 15:35:36 -0700

    edit app js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <h1>Phase 5 Project</h1>
     </div>
   );
 }
