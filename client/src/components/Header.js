3b7fc130c8ba08a86329c596db8c3cc876eb1655
Joseph McPartland <joemcpartland@gmail.com>
2023-09-13 18:27:27 -0700

    fix mobile menu links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,171 +1,203 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import Link from '@mui/material/Link';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+// import AdbIcon from '@mui/icons-material/Adb';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#815d72',
-    },
-    secondary: {
-      main: '#a9b650',
-    }
-  },
-});
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact'];
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
+  const title = "Long Covid Support Community"
+  const sections = [
+    { title: "Home", url: "/" },
+    { title: "My Posts", url:"/posts"},
+    { title: "All Posts", url: "/all-posts" },
+    { title: "Resources", url: "/resources" },
+  ];
 
-const sections = [
-  { title: 'Home', url: '/' },
-  { title: 'My Posts', url:'/posts'},
-  { title: 'All Posts', url: '/all-posts' },
-  { title: 'Resources', url: '/resources' },
-];
-
-function Header(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const title = "Long Covid Support Community";
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        {title}
-      </Typography>
-      <Divider />
-      <List>
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-        {/* {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))} */}
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-        {sections.map((section) => (
-          <ListItem key={section} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              {/* <ListItemText primary={section} /> */}
-              <Link
-                  color="#ffffff"
-                  key={section.title}
-                  underline="none"
-                  href={section.url}
-                  sx={{ p: 1, flexShrink: 0 }}
-                >
-                  {section.title}
-                </Link>
-            </ListItemButton>
-          </ListItem>
-        ))}
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#815d72',
+      },
+      secondary: {
+        main: '#b0ac89',
+      },
+    },
+  });
 
-      </List>
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
     <ThemeProvider theme={theme}>
-
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <Typography
             variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              // fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.1rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
           >
             {title}
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
 
-            {/* {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
-            ))} */}
-
-            {sections.map((section) => (
-              <Button>
-                <Link
-                  color="#ffffff"
-                  key={section.title}
-                  underline="none"
-                  href={section.url}
-                  sx={{ p: 1, flexShrink: 0 }}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {sections.map((sec) => (
+                <MenuItem 
+                  key={sec.title} 
+                  // onClick={handleCloseNavMenu}
+                  component={Link} to={sec.url}
                 >
-                  {section.title}
-                </Link>
-              </Button>
+                  {/* <Link
+                    color="inherit"
+                    noWrap
+                    key={sec.title}
+                    // variant="body1"
+                    underline="none"
+                    href={sec.url}
+                    sx={{ p: 1, flexShrink: 0 }}
+                  > */}
+                    <Typography textAlign="left">
+                      {sec.title}
+                    </Typography>
+                  {/* </Link> */}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+          <Typography
+            variant="h7"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              // fontFamily: 'monospace',
+              // letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            {title}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {sections.map((sec) => (
+              <Link
+                color="inherit"
+                noWrap
+                key={sec.title}
+                // variant="body1"
+                underline="none"
+                href={sec.url}
+                sx={{ p: 1, flexShrink: 0 }}
+              >
+                <Typography textAlign="left">{sec.title}</Typography>
+              </Link>
             ))}
+          </Box>
 
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
         </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-      </Box>
-    </Box>
-
+      </Container>
+    </AppBar>
     </ThemeProvider>
   );
 }
-
-Header.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
-
 export default Header;
